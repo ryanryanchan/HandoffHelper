2c8e315cc294836b7c9ad5ba36a5190851c48e80
RYAN CHAN <ryanchan1622@yahoo.com>
2016-11-20 19:44:00 -0800

    fix

--- app/src/main/java/com/example/ryanchan/handoffhelper/MainActivity.java
+++ app/src/main/java/com/example/ryanchan/handoffhelper/MainActivity.java
@@ -12,7 +12,6 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.LinearLayout;
